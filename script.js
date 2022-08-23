d65f4b860f231a885e6d48787560d4d8ffac9cf4
Arnold Cubici-Jones <arnoldcubicijones@hotmail.co.uk>
2022-08-23 12:33:08 +0100

    DOM fnctns to show users option on scrn, theGame fnctn adapted

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function getComputerChoice() { // generates random number and attaches to rock, paper, or scissors
     let num = Math.floor(Math.random() * 3 );
     
-    console.log(num);
     var compchoice = null;
     
     if (num == 1) {
@@ -18,19 +17,60 @@ function getComputerChoice() { // generates random number and attaches to rock, 
 
     console.log(getComputerChoice());
 
- function rock_fun() {
+ 
 
-    return console.log("You chose Rock!");
- }
 
- function paper_fun() {
-    return console.log("You chose Paper!");
- }
+function theGame(playerSelection, computerSelection) {
+/* rock over scissors, rock loses to paper
+scissors loses over rock, wins over paper
+paper wins over rock, loses over scissors */
+let result = null;
+let win = "You won!";
+let lose = "You loss :(";
 
- function scissors_fun() {
-    return console.log("You chose Scissors!");
- }
+if(playerSelection == "rock" && computerSelection == "scissors") {
+    result = win;
+    
+} else if (playerSelection == "rock" && computerSelection == "paper") {
+    result = lose;
+    
+} else if (playerSelection == "scissors" && computerSelection == "rock") {
+    result = lose;
+    
+} else if (playerSelection == "scissors" && computerSelection == "paper") {
+    result = win;
+    
+} else if (playerSelection == "paper" && computerSelection == "rock") {
+    result = win;
+    
+} else if (playerSelection == "paper" && computerSelection == "scissors") {
+    result = lose;
 
-function thegame(playerSelection, computerSelection) {
-
+return console.log(result);
 }
+} // end function theGame
+
+
+function rock_fun() {
+    let playerChoice = "rock";
+    document.getElementById("playerc").innerHTML = "Rock!";
+    let computerChoice = getComputerChoice();
+    let winner = theGame(playerChoice, computerChoice);
+     }
+    
+     function paper_fun() {
+    let playerChoice = "paper";
+    document.getElementById("playerc").innerHTML = "Paper!";
+    let computerChoice = getComputerChoice();
+    theGame();
+        return console.log("You chose Paper!");
+     }
+    
+     function scissors_fun() {
+    document.getElementById("playerc").innerHTML = "Scissors!";
+    let computerChoice = getComputerChoice();
+    let playerChoice = "scissors";
+        return console.log("You chose Scissors!");
+     }
+
+     
