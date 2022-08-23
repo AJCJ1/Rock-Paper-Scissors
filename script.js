99a6e29df7c99166a4ebe992331e3134ff054c88
Arnold Cubici-Jones <arnoldcubicijones@hotmail.co.uk>
2022-08-23 13:17:06 +0100

    Make theGame function operational, display result on viewport

--- script.js
+++ script.js
@@ -17,65 +17,67 @@ function getComputerChoice() { // generates random number and attaches to rock, 
 
     console.log(getComputerChoice());
 
- 
-
-
-function theGame(playerSelection, computerSelection) {
-/* rock over scissors, rock loses to paper
-scissors loses over rock, wins over paper
-paper wins over rock, loses over scissors */
-let result = null;
-let win = "You won!";
-let lose = "You loss :(";
-
-if(playerSelection == "rock" && computerSelection == "scissors") {
-    result = win;
-    
-} else if (playerSelection == "rock" && computerSelection == "paper") {
-    result = lose;
-    
-} else if (playerSelection == "scissors" && computerSelection == "rock") {
-    result = lose;
-    
-} else if (playerSelection == "scissors" && computerSelection == "paper") {
-    result = win;
-    
-} else if (playerSelection == "paper" && computerSelection == "rock") {
-    result = win;
-    
-} else if (playerSelection == "paper" && computerSelection == "scissors") {
-    result = lose;
-
-return console.log(result);
-}
-} // end function theGame
 
 
 function rock_fun() {
-    let playerChoice = "rock";
+    let playerChoice = "Rock";
     document.getElementById("playerc").innerHTML = "Rock";
     let computerChoice = getComputerChoice();
     document.getElementById("compc").innerHTML = computerChoice;
     let winner = theGame(playerChoice, computerChoice);
+    document.getElementById("winner").innerHTML = theGame(playerChoice, computerChoice);
+        return;
      }
     
      function paper_fun() {
-    let playerChoice = "paper";
+    let playerChoice = "Paper";
     document.getElementById("playerc").innerHTML = "Paper";
     let computerChoice = getComputerChoice();
     document.getElementById("compc").innerHTML = computerChoice;
-    theGame();
-        return console.log("You chose Paper!");
+    let winner = theGame(playerChoice, computerChoice);
+    document.getElementById("winner").innerHTML = theGame(playerChoice, computerChoice);
+        return;
      }
     
      function scissors_fun() {
-    document.getElementById("playerc").innerHTML = "Scissors";
+    let playerChoice = "Scissors";
     let computerChoice = getComputerChoice();
+    let winner = theGame(playerChoice, computerChoice);
+
+    document.getElementById("playerc").innerHTML = "Scissors";
     document.getElementById("compc").innerHTML = computerChoice;
-    let playerChoice = "scissors";
-        return console.log("You chose Scissors!");
+
+    document.getElementById("winner").innerHTML = theGame(playerChoice, computerChoice);
+        return;
      }
 
+     function theGame(playerSelection, computerSelection) {
+        /* rock over scissors, rock loses to paper
+        scissors loses over rock, wins over paper
+        paper wins over rock, loses over scissors
+        otherwise draw */
+        let result = (playerSelection + computerSelection)
+      
+        if((playerSelection == "Rock") && (computerSelection == "Scissors")) {
+            result = ("Result: You won!");
+              
+        } else if ((playerSelection == "Rock") && (computerSelection == "Paper")) {
+            result = ("Result: You lost :(");
+            
+        } else if ((playerSelection == "Scissors") && (computerSelection == "Rock")) {
+            result = ("Result: You lost :(");
 
+        } else if ((playerSelection == "Scissors") && (computerSelection == "Paper")) {
+            result = ("Result: You won!");
+            
+        } else if ((playerSelection == "Paper") && (computerSelection == "Rock")) {
+            result = ("Result: You won!");
+            
+        } else if ((playerSelection == "Paper") && (computerSelection == "Scissors")) {
+            result = ("Result: You lost :(");
+        } else {
+            result = ("Result: Draw");
+        } // end conditional
+        return result;
 
-     // Adapt main buttons to display computers choice
+        } // end function theGame
