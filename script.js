25edf3eb117d8c9706c9e14e20e897b52f362e1c
Arnold Cubici-Jones <arnoldcubicijones@hotmail.co.uk>
2022-08-25 16:11:37 +0100

    Add reset function to display overlay and reset tally

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ var result = "ERROR";
 // end of declarations
 
 function getComputerChoice() { // generates random number and attaches to rock, paper, or scissors
-    let num = Math.floor(Math.random() * 3 );
-    
-    var compchoice = null;
+
+let num = Math.floor(Math.random() * 3 );  
+let compchoice = null;
     
     if (num == 1) {
     compchoice = "Rock";
@@ -19,12 +19,11 @@ function getComputerChoice() { // generates random number and attaches to rock, 
     compchoice = "Scissors"
     } // end of if
     
-     return compchoice;
+return compchoice;
     
     } // end random computer's choice function
 
-    console.log(getComputerChoice());
-
+console.log(getComputerChoice());
 
 
 function rock_fun() {
@@ -33,80 +32,92 @@ function rock_fun() {
     let computerChoice = getComputerChoice();
     document.getElementById("compc").innerHTML = computerChoice;
     let winner = theGame(playerChoice, computerChoice);
-    document.getElementById("winner").innerHTML = theGame(playerChoice, computerChoice);
-        return;
+    document.getElementById("result").innerHTML = theGame(playerChoice, computerChoice);
+return;
      }
     
-     function paper_fun() {
+function paper_fun() {
     let playerChoice = "Paper";
-    document.getElementById("playerc").innerHTML = "Paper";
     let computerChoice = getComputerChoice();
-    document.getElementById("compc").innerHTML = computerChoice;
     let winner = theGame(playerChoice, computerChoice);
-    document.getElementById("winner").innerHTML = theGame(playerChoice, computerChoice);
-        return;
+    document.getElementById("playerc").innerHTML = "Paper";
+    document.getElementById("compc").innerHTML = computerChoice;
+    document.getElementById("result").innerHTML = theGame(playerChoice, computerChoice);
+return;
      }
     
-     function scissors_fun() {
+function scissors_fun() {
     let playerChoice = "Scissors";
     let computerChoice = getComputerChoice();
     let winner = theGame(playerChoice, computerChoice);
-
     document.getElementById("playerc").innerHTML = "Scissors";
     document.getElementById("compc").innerHTML = computerChoice;
-
-    document.getElementById("winner").innerHTML = theGame(playerChoice, computerChoice);
-        return;
+    document.getElementById("result").innerHTML = theGame(playerChoice, computerChoice);
+return;
      } // end button functions
 
 
-     function theGame(playerSelection, computerSelection) {
+function theGame(playerSelection, computerSelection) {
         /* rock over scissors, rock loses to paper
         scissors loses over rock, wins over paper
         paper wins over rock, loses over scissors
         otherwise draw */
         
-      
-        if((playerSelection == "Rock") && (computerSelection == "Scissors")) {
+// conditional that selects the winner
+    if((playerSelection == "Rock") && (computerSelection == "Scissors")) {
             result = ("Result: You won!");
               
-        } else if ((playerSelection == "Rock") && (computerSelection == "Paper")) {
+    } else if ((playerSelection == "Rock") && (computerSelection == "Paper")) {
             result = ("Result: You lost :(");
             
-        } else if ((playerSelection == "Scissors") && (computerSelection == "Rock")) {
+    } else if ((playerSelection == "Scissors") && (computerSelection == "Rock")) {
             result = ("Result: You lost :(");
 
-        } else if ((playerSelection == "Scissors") && (computerSelection == "Paper")) {
+    } else if ((playerSelection == "Scissors") && (computerSelection == "Paper")) {
             result = ("Result: You won!");
             
-        } else if ((playerSelection == "Paper") && (computerSelection == "Rock")) {
+    } else if ((playerSelection == "Paper") && (computerSelection == "Rock")) {
             result = ("Result: You won!");
             
-        } else if ((playerSelection == "Paper") && (computerSelection == "Scissors")) {
+    } else if ((playerSelection == "Paper") && (computerSelection == "Scissors")) {
             result = ("Result: You lost :(");
-        } else {
+    } else {
             result = ("Result: Draw");
-        } // end conditional
+    } 
+// end conditional
 
+// Tally counter conditional
         if (result == ("Result: You lost :(")) {
             computerTally += 0.5;
             document.getElementById("cTally").innerHTML = ("Computer: " + computerTally);
         } else if (result == ("Result: You won!")) {
             playerTally += 0.5;
             document.getElementById("pTally").innerHTML = ("Player: " + playerTally);
-        } else {
-
         }
-
+// end conditional
+// upto5 conditional
         if (playerTally >= 5) {
-            document.getElementById("final").innerHTML = ("Result: Game over! You win!");
+        /*    document.getElementById("final").innerHTML = ("Result: Game over! You win!"); */
+            document.getElementById("game_over_overlay").innerHTML = "Game over, You win!"
+            document.getElementById("game_over_overlay").setAttribute('style','display:inline-block');
+
         } else if (computerTally >= 5) {
-            document.getElementById("final").innerHTML = ("Result: Game over! You lose...");
+            document.getElementById("game_over_overlay").innerHTML = "Game over, You lose..."
+            document.getElementById("game_over_overlay").setAttribute('style','display:inline-block');
         }
-        return result;
+//end conditional
 
-        } // end theGame function
+return result;
+} // end theGame function
 
+function reset() {
+    document.getElementById("game_over_overlay").style.display = "none";
+    computerTally = 0;
+    playerTally = 0;
+}
+
+
+//Add function to display GAME OVER after one tally gets to 5
         
 
    
